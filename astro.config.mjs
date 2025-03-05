0e07d65a75bda7815d994422e7ce6533bef81a4b
Emilio Santillana Précoma <emiprec77@gmail.com>
2025-03-05 14:33:14 -0600

    Configure github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   output: "static",
   site: 'https://emisanpre.github.io',
+  base: '/github-pages',
 
   integrations: [svelte()],
 
